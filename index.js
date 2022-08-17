51dd0d90314f56326ba834a71c07aa1861464b87
Guntash Kaur <83450329+GuntashKaur@users.noreply.github.com>
2022-08-17 18:58:12 +0530

    updated

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ checkButton.addEventListener("click", clickHandler);
 function clickHandler(){
     const dob = dateOfBirth.value;
     const sum = calculateSum(dob);
+
+    compareValues(sum, luckyNumber.value);
     
 }
 
+function calculateSum(dob){
+
+    dob = dob.replaceAll("-", "");
+    let add = 0;
+    for(let i=0; i<dob.length ; i++){
+        add = add + Number(dob.charAt(i));
+    }
+    return add;
+
+}
+
+function compareValues(sum,luckyNumber){
+
+    if(sum%luckyNumber===0){
+        console.log("Your birthday is lucky")
+    }
+    else{
+        console.log("Your birthday is not lucky")
+    }
+
+}
